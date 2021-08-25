0c385d328853214a64b247de998d661f9ae4f485
nickjang <nickjang234@gmail.com>
2021-08-25 16:54:50 -0500

    add editprofile & myitem pages

---
+++ src/assets/account_data.tsx
@@ -0,0 +1,11 @@
+import imgAvatar from "./imgs/seller3.png";
+
+const account_data = {
+    name: "Damien King",
+    price: "30.000PUML",
+    custom_url: "rarible.com/damienking",
+    email: "damienking@gmail.com",
+    img: imgAvatar
+}
+
+export default account_data;
--- src/pages/MyItems.tsx
+++ src/pages/MyItems.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import back from "assets/imgs/hot-back.svg";
+import { Image, Button } from "react-bootstrap";
+import account_data from "assets/account_data";
+import Layout from "components/Layout";
 
 interface MyItemProps { }
 
 const MyItems: React.FC<MyItemProps> = () => {
-    
+    const layoutView = useRef(null);
     return (
-        <div style={{width: "100%", backgroundImage: back }}>asdfawefsdfawefadfwef</div>
+        <Layout className="myitems-container" ref={layoutView}>
+            <div className="d-flex flex-column align-items-center">
+                <Image src={back} className="background-item"></Image>
+                <Image src={account_data.img} className="avatar"></Image>
+                <h1 className="user-name">{account_data.name}</h1>
+                <Button className="mr-2 mr-lg-4 btn-primary">Edit Profile</Button>
+            </div>
+        </Layout>
     );
 }
 
