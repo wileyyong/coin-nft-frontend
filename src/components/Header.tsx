23e97d2a4e05d252430bc90b3ca9970f8f56894d
nickjang <nickjang234@gmail.com>
2021-08-30 07:34:27 -0500

    update results.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Image, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Image, NavDropdown, Button } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "store/hooks";
 
@@ -55,71 +55,67 @@ const Header: React.FC<HeaderProps> = () => {
           <span />
           <span />
         </Navbar.Toggle>
-        <Navbar.Brand as={Link} to="/" className="navbar-brand">
-          <Image className="logo" src={logoImg} alt="logo" />
+        <Navbar.Brand as={Link} to="/">
+          <img className="logo" src={logoImg} alt="logo" />
         </Navbar.Brand>
-
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Item >
+            <Nav.Item>
               <Nav.Link
                 eventKey="1"
                 as={Link}
                 to="/"
-                className="mr-lg-4"
+                className="mr-lg-3"
                 active={
-                  location.pathname === "/"
+                  location.pathname === "/home" || location.pathname === "/"
                 }
               >
                 Home
               </Nav.Link>
             </Nav.Item>
-
-            <Nav.Item >
+            <Nav.Item>
               <Nav.Link
                 eventKey="2"
                 as={Link}
                 to="/items"
-                className="mr-lg-4"
-                active={
-                  location.pathname === "/items"
-                }
+                className="mr-lg-3"
+                active={location.pathname === "/items"}
               >
                 My Items
               </Nav.Link>
             </Nav.Item>
-
-            <NavDropdown
-              title={getDropdownAvatar()}
-              id="header-nav-dropdown"
-              alignRight={true}
-            >
-              <div className="px-4">
-                {userInfo && userInfo.name && (
-                  <B1NormalTextTitle className="mb-2 mt-1">
-                    {userInfo.name}
-                  </B1NormalTextTitle>
-                )}
-                <FlexAlignCenterDiv className="user-detail">
-                  <Image src={avatarImg}></Image>
-                  <div className="ml-2">
-                    <NormalTextTitle>Balance</NormalTextTitle>
-                    <SmallTextTitleGrey>30.000PUML</SmallTextTitleGrey>
+            <Nav.Item className="d-flex pr-4 buttons">
+                <NavDropdown
+                  title={getDropdownAvatar()}
+                  id="header-nav-dropdown"
+                  alignRight={true}
+                >
+                  <div className="px-4">
+                    {userInfo && userInfo.name && (
+                      <B1NormalTextTitle className="mb-2 mt-1">
+                        {userInfo.name}
+                      </B1NormalTextTitle>
+                    )}
+                    <FlexAlignCenterDiv>
+                      <div className="ml-2">
+                        <NormalTextTitle>Balance</NormalTextTitle>
+                        <SmallTextTitleGrey>10000 ETH</SmallTextTitleGrey>
+                      </div>
+                    </FlexAlignCenterDiv>
                   </div>
-                </FlexAlignCenterDiv>
-              </div>
 
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} to="/items">
-                My Items
-              </NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/profile">
-                Edit Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item>
-                Disconnect
-              </NavDropdown.Item>
-            </NavDropdown>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item as={Link} to="/items">
+                    My Items
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/profile">
+                    Edit Profile
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    Disconnect
+                  </NavDropdown.Item>
+                </NavDropdown>
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
