c9c069f11c5d9b787442ca3c55fa3d6ebc0bcc74
nickjang <nickjang234@gmail.com>
2021-09-20 16:26:31 -0500

    update logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Image } from "react-bootstrap";
 
-import logo_footer from "assets/imgs/logo.png";
+import logo_footer from "assets/imgs/puml-logo-footer.png";
 import image1 from "assets/imgs/path.png";
 import image2 from "assets/imgs/twitter.png";
 import image3 from "assets/imgs/youtube.png";
@@ -11,6 +11,10 @@ const Footer = () => {
       <div>
         <div className="d-flex align-items-center justify-content-center footer-top">
           <Image className="logo-image" src={logo_footer} alt="logo"></Image>
+          <div className="d-flex flex-column">
+            <div className="font-weight-bold font-italic">PUML</div>
+            <div className="logo-title text-primary font-weight-bold font-italic">Better Health</div>
+          </div>
         </div>
         <div className="d-flex flex-wrap justify-content-center">
           <div className="item-content font-weight-bold">Top Sellers</div>
@@ -19,7 +23,7 @@ const Footer = () => {
           <div className="item-content font-weight-bold">Privacy Policy</div>
           <div className="item-content font-weight-bold">Terms and Condtions</div>
         </div>
-        <hr className="divider"/>
+        <hr className="divider" />
 
         <div className="d-flex footer-bottom justify-content-between align-items-center">
           <div className="text-black pb-2">© 2021 PUML All rights reserved</div>
@@ -29,7 +33,7 @@ const Footer = () => {
             <Image className="link-img pr-4" src={image3} alt="alt"></Image>
           </div>
         </div>
-        
+
       </div>
     </footer>
   );
